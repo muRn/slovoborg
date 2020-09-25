145e0ec7a02a5373c63581dcbca818f739306a5a
Artem Grankin <agrankin@klta.net>
2020-09-26 00:57:56 +0200

    initial project state based on spring initializr - lombok - spring web - thymeleaf - spring security - spring data jpa - spring boot devtools

---
+++ src/main/java/org/slovob/slovoborg/SlovoborgApplication.java
@@ -0,0 +1,13 @@
+package org.slovob.slovoborg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlovoborgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlovoborgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/slovob/slovoborg/SlovoborgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.slovob.slovoborg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlovoborgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
