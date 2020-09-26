7a7658be2bd27605f25fbaf73d0ccaf92cdbf89b
Artem Grankin <agrankin@klta.net>
2020-09-26 22:28:48 +0200

    simplest word definition entity and html form to submit new definition

---
+++ src/main/java/org/slovob/slovoborg/definition/Definition.java
@@ -0,0 +1,23 @@
+package org.slovob.slovoborg.definition;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Definition {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String word;
+    private String definition;
+    private String example;
+    private String submittedBy;
+    private LocalDate submittedOn;
+    private int likes;
+    private int dislikes;
+}
---
+++ src/main/java/org/slovob/slovoborg/definition/DefinitionController.java
@@ -0,0 +1,26 @@
+package org.slovob.slovoborg.definition;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+
+@Controller
+@RequestMapping("/definition")
+public class DefinitionController {
+    private DefinitionRepository repo;
+
+    @Autowired
+    public DefinitionController(DefinitionRepository repo) {
+        this.repo = repo;
+    }
+
+    @PostMapping
+    public String saveDefinition(Definition definition) {
+        definition.setSubmittedOn(LocalDate.now());
+        repo.save(definition);
+        return "index";
+    }
+}
