9f85c26debcba60b8256835a7ba33e5801f7ab26
Artem Grankin <agrankin@klta.net>
2020-09-30 02:27:33 +0200

    frontend changes to show like/dislike button toggle

--- src/main/resources/static/js/opinion.js
+++ src/main/resources/static/js/opinion.js
@@ -2,9 +2,29 @@ function postOpinion(opinion) {
     var request = new XMLHttpRequest();
     request.open('POST', '/opinion', true);
     request.setRequestHeader('Content-Type', 'application/json');
+    request.onload = function() {
+        changeCount(opinion);
+    };
+    request.onerror = function() {
+        console.log('something went wrong while POSTing opinion');
+    };
     request.send(JSON.stringify(opinion));
 }
 
 function changeCount(opinion) {
+    let action = opinion.opinion === 1 ? 'like' : 'dislike';
 
+    let buttonId = action + '_btn_' + opinion.definitionId;
+    let countId = action + 's_cnt_' + opinion.definitionId;
+
+    let button = document.querySelector('#' + buttonId);
+    let count = document.querySelector('#' + countId);
+
+    if (button.classList.contains('primary')) {
+        button.classList.remove('primary');
+        count.textContent = count.textContent - 1;
+    } else {
+        button.classList.add('primary');
+        count.textContent = count.textContent - -1;
+    }
 }
