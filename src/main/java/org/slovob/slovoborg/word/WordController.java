2c5b3c4187d60f720be30e66c5617d897574d808
Artem Grankin <agrankin@klta.net>
2020-10-02 00:09:07 +0200

    new WordController could be used to see definitions that are not approved yet. Fixed.

--- src/main/java/org/slovob/slovoborg/word/WordController.java
+++ src/main/java/org/slovob/slovoborg/word/WordController.java
@@ -1,5 +1,6 @@
 package org.slovob.slovoborg.word;
 
+import org.slovob.slovoborg.definition.Definition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Optional;
+import java.util.List;
 
 @Controller
 @RequestMapping("/word")
@@ -21,8 +22,8 @@ public class WordController {
 
     @GetMapping
     public String showWordDefinitions(@RequestParam long id, Model model) {
-        Optional<Word> wordOpt = repo.findById(id);
-        wordOpt.ifPresent(word -> model.addAttribute(word.getDefinitions()));
+        List<Definition> definitions = repo.findApprovedDefinitionsByWordId(id);
+        model.addAttribute(definitions);
         return "index";
     }
 }
--- src/main/java/org/slovob/slovoborg/word/WordRepository.java
+++ src/main/java/org/slovob/slovoborg/word/WordRepository.java
@@ -1,11 +1,17 @@
 package org.slovob.slovoborg.word;
 
+import org.slovob.slovoborg.definition.Definition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface WordRepository extends JpaRepository<Word, Long> {
     public Optional<Word> findByWord(String word);
+
+    @Query("select d from Word w join w.definitions d where d.approved=true and w.id=?1")
+    public List<Definition> findApprovedDefinitionsByWordId(long id);
 }
