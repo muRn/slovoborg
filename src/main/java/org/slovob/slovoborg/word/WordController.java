9ba43649a672922c7621bce32704d93bd0928b23
Artem Grankin <agrankin@klta.net>
2020-11-13 19:39:18 +0100

    /word pages were missing any data since switching to DefinitionDto, fixed it

--- src/main/java/org/slovob/slovoborg/word/WordController.java
+++ src/main/java/org/slovob/slovoborg/word/WordController.java
@@ -1,7 +1,10 @@
 package org.slovob.slovoborg.word;
 
 import org.slovob.slovoborg.definition.Definition;
+import org.slovob.slovoborg.definition.DefinitionDto;
+import org.slovob.slovoborg.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/word")
@@ -21,9 +25,12 @@ public class WordController {
     }
 
     @GetMapping
-    public String showWordDefinitions(@RequestParam long id, Model model) {
+    public String showWordDefinitions(@RequestParam long id, Model model, @AuthenticationPrincipal User user) {
         List<Definition> definitions = repo.findApprovedDefinitionsByWordId(id);
-        model.addAttribute(definitions);
+        List<DefinitionDto> definitionDtoList = definitions.stream()
+                .map(x -> DefinitionDto.fromDefinition(x, user))
+                .collect(Collectors.toList());
+        model.addAttribute(definitionDtoList);
         return "index";
     }
 }
